8b5dcb44def5919c74d9d2bec480712a7516e821
DevShasa <shasa.thuo@gmail.com>
2024-04-30 04:01:05 +0300

    chore: fix https bug

--- src/utils/weatherUrls.tsx
+++ src/utils/weatherUrls.tsx
@@ -4,7 +4,7 @@ export const urls = {
     fiveDayForecast: (lat:string, lon:string) =>
         `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=dd03db6b19872c7cb9d5af234821dd03`,
     airQuality:(lat:string, lon:string)=>
-        `http://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=dd03db6b19872c7cb9d5af234821dd03&units=metric`
+        `https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=dd03db6b19872c7cb9d5af234821dd03&units=metric`
 
 };
 
